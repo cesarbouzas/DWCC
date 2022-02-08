f52eb14b3d99e99a2de22d177f5aad6cc6e5e93c
cesar <cesarbouzaspaw@gmail.com>
2022-02-08 20:45:37 +0100

    no copiar

--- 005_Tema_5/tarea5.js
+++ 005_Tema_5/tarea5.js
@@ -1,7 +1,46 @@
+
+
+
+
+function validarNombreYapellidos(){
+    let nombre=document.getElementById("nombre").value;
+    let apellidos=document.getElementById("apellidos").value;
+    let re=new RedExp('^[a-zA-Z]+$');
+    correcto=re.test(nombre) && re.test(apellidos);
+    //probamos como match
+    if(!corecto){
+        
+      mostrarError();
+      PonerFoco();  
+      return false;
+    }
+    console.log("cumple");
+    return true;
+    //console.log(event.target);
+}
+
+
+
+
+function pasarMayus(event){
+ let elemento=event.target;
+ elemento.value=elemento.value.toUpperCase();
+ //console.log(event.target);
+
+}
+
+
+
+//el event lo rellena con el evento que venga
 function validar(event){
-    valido=false;
+    valido=true;
+    valido=valido && validarNombreYapellidos();
+    //valido=valido && validarEmail();
+    //valido=validarEmail()&&valido;
+
     if(!valido){
         console.log("false");
+        //console.log("event");
         event.preventDefault();
     }
 }
@@ -12,8 +51,13 @@ function validar(event){
 function ready(){
     let formulario=document.getElementById("formulario");
     //console.log(botonEnviar);
-    formulario.addEventListener("submit",validar)
+    let nombre=document.getElementById("nombre");
+    let apellidos=document.getElementById("apellidos");
 
+
+    formulario.addEventListener("submit",validar);
+    nombre.addEventListener("blur",pasarMayus);
+    apellidos.addEventListener("blur",pasarMayus);
     
 
 }
