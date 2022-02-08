519a306786460e331c4d72f7a79e80e48e034a81
cesar <cesarbouzaspaw@gmail.com>
2022-02-08 19:42:22 +0100

    monos

--- 005_Tema_5/tarea5.js
+++ 005_Tema_5/tarea5.js
@@ -1,5 +1,20 @@
-<script>
+function validar(event){
+    valido=false;
+    if(!valido){
+        console.log("false");
+        event.preventDefault();
+    }
+}
 
+
+
+
+function ready(){
+    let formulario=document.getElementById("formulario");
+    //console.log(botonEnviar);
+    formulario.addEventListener("submit",validar)
 
     
-</script>
+
+}
+document.addEventListener("DOMContentLoaded",ready)
