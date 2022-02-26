0c343ed160ccd1ca6baa9c4c0c7d83fd2286b469
cesar <cesarbouzaspaw@gmail.comr>
2022-02-26 17:41:45 +0100

    ddd

--- 005_Tema_5/Emplos_tema/E1.js
+++ 005_Tema_5/Emplos_tema/E1.js
@@ -1,12 +1,11 @@
 console.log(document.forms.length);
-let nombre = document.forms[0].elements[0].value;
+
 
 /*
+let nombre = document.forms[0].elements[0].value;
 let apellido1 = document.getElementById("apellido").value;
 let apellido2 = document.forms[1].elements[1].value;
-*/
 console.log(nombre);
-//console.log(apellido1);
-//console.log(apellido2)
-
-//document.getElementById("apellido2").innerHTML;
+console.log(apellido1);
+console.log(apellido2)
+document.getElementById("apellido2").innerHTML;
